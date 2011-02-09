0c5bed09e44c1d945a9fbef6dfd7a8332ac7f7e7
sebwordpress <sebwordpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-09 10:35:57 +0000

    ...version
    git-svn-id: https://plugins.svn.wordpress.org/the-welcomizer/tags/1.3.2@342995 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twiz-index.php
+++ twiz-index.php
@@ -1,7 +1,7 @@
 <?
 /*
 Plugin Name: The Welcomizer
-Version: 1.3.1.9
+Version: 1.3.2
 Plugin URI: http://www.sebastien-laframboise.com/wordpress/plugins-wordpress/the-welcomizer
 Description: Currently activated on the Homepage, this plugin allows you to easily add 'Smart' moves and jQuery effects to virtually any HTML element that has an attribute ID.(e.g. div id="sidebar"). Enjoy!
 Author: Sebastien Laframboise
