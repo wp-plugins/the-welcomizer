0f6c83c62269973511ad225ffd854ea5b0749ed7
sebwordpress <sebwordpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-02 03:42:39 +0000

    version 1.3.3.8 (Modified css)
    git-svn-id: https://plugins.svn.wordpress.org/the-welcomizer/tags/1.3.3.8@354004 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ twiz-index.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: The Welcomizer
+Version: 1.3.3.8
+Plugin URI: http://www.sebastien-laframboise.com/wordpress/plugins-wordpress/the-welcomizer
+Description: Activated on the homepage, categories or pages, this plugin allows you to add 'Smart' moves and jQuery effects to virtually any HTML element that has an attribute ID. (e.g. div id="sidebar"). Enjoy!
+Author: Sebastien Laframboise
+Author URI: http://www.sebastien-laframboise.com
+License: GPL2
+*/
+
+/*  Copyright 2011  Sebastien Laframboise  (email:wordpress@sebastien-laframboise.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+    /********************
+    * --- The Classes ---
+    *********************/
+    
+    require_once(dirname(__FILE__).'/includes/twiz-shd.class.php'); 
+    require_once(dirname(__FILE__).'/includes/twiz.class.php'); 
+    
+    /******************
+    * --- Functions ---
+    *******************/
+    
+    /* Create the necessary for the installation */
+    function twizInstall() {
+        
+        $myTwiz  = new Twiz();
+        $myTwiz->install();
+    }
+    
+    /* uninstall the plugin, drop table etc... */
+    function twizUninstall() {
+    
+        $myTwiz  = new Twiz();
+        $myTwiz->uninstall();
+    }
+
+    /* Add a menu link under theme menu. */
+    function twizMenu() {
+    
+        add_theme_page(__('The Welcomizer', 'the-welcomizer'), __('The Welcomizer', 'the-welcomizer'), 6, 'the-welcomizer', 'twizMainPage');
+    }
+        
+    /* Admin page */
+    function twizMainPage() {     
+    
+        $myTwiz = new Twiz();
+        echo($myTwiz->twizIt());
+    }
+    
+    /* Frontend code */
+    function twizFrontEnd() {
+    
+        $myTwiz  = new Twiz();
+        echo($myTwiz->getFrontEnd());
+    }
+    
+    /****************
+    * --- Actions ---
+    *****************/
+    
+    /* register installation hooks */
+    register_activation_hook( __FILE__,  'twizInstall' );
+    register_deactivation_hook( __FILE__,  'twizUninstall' );    
+
+    /* Set the multi-language file, english is the standard. */
+    load_plugin_textdomain( 'the-welcomizer', false, dirname( plugin_basename( __FILE__ ) ).'/languages/' ); 
+    
+    /* Enqueue style in admin */
+    if(is_admin()){
+        wp_enqueue_style('twiz-style', WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)).'includes/twiz-style.css');
+        wp_enqueue_style('twiz-style-b', WP_PLUGIN_URL.'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)).'includes/import/client/fileuploader.css');
+    }
+    
+    /* Add the menu link */
+    add_action('admin_menu', 'twizMenu');
+    
+    /* add the Frontend generated code */    
+    add_filter('wp_footer', 'twizFrontEnd');
+?>
