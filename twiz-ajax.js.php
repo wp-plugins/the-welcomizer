09c1b243a461b1624e397a69d473c27bd2af2c75
sebwordpress <sebwordpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-05 17:59:40 +0000

    Fixed translations
    git-svn-id: https://plugins.svn.wordpress.org/the-welcomizer/trunk@433673 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twiz-ajax.js.php
+++ twiz-ajax.js.php
@@ -6,6 +6,9 @@ else{
     require_once('../../../wp-load.php');
 }
 
+// Set the multi-language file, english is the standard.
+load_plugin_textdomain( 'the-welcomizer', false, dirname( plugin_basename( __FILE__ ) ).'/languages/' ); 
+        
 require_once(dirname(__FILE__).'/includes/twiz.class.php'); 
 require_once(dirname(__FILE__).'/includes/twiz.ajax.class.php'); 
 
